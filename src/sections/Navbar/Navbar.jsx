ab8d41a22b83f663181142942ee73706ef79df85
Norchh <a.norawich@gmail.com>
2023-10-06 15:21:04 +0700

    update responsive and func for Navbar

--- src/sections/Navbar/Navbar.jsx
+++ src/sections/Navbar/Navbar.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import images from "../../constants/images";
 import { menulist } from "../../constants/data";
-// import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-  // const [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const menuToggleHandler = () => {
+    setToggleMenu(!toggleMenu);
+    console.log(toggleMenu);
+  };
 
   return (
     <>
@@ -17,6 +22,26 @@ const Navbar = () => {
           </div>
           <div className="navbar__menu">
             <ul className="navbar__menu__list">
+              {menulist.map((item) => (
+                <li key={item.title} className="text__upper text__light">
+                  <a href={item.path}>{item.title}</a>
+                </li>
+              ))}
+            </ul>
+            <div className="menu-btn" onClick={menuToggleHandler}>
+              <FaBars color="white" fontSize="3rem" />
+            </div>
+          </div>
+
+          {/* SMALL SCREEN */}
+          <div className={`navbar__menu__small ${toggleMenu ? "active" : ""}`}>
+            <FaTimes
+              color="white"
+              fontSize="3.3rem"
+              className="i-close"
+              onClick={menuToggleHandler}
+            />
+            <ul className="navbar__menu__small__list">
               {menulist.map((item) => (
                 <li key={item.title} className="text__upper text__light">
                   <a href={item.path}>{item.title}</a>
